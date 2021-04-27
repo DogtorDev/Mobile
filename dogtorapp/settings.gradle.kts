6f16d59dc9ab0f86601494981c931fbc0879d0a8
Manuel Gonzalez Blanco <manuel.gonzalez.freelancer@gmail.com>
2021-04-27 13:00:17 +0200

    Initial Android project

---
+++ dogtorapp/settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "dogtorapp"
+include(":androidApp")
+include(":shared")
